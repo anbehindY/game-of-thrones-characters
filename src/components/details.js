2ef0d1b47c78fb3f11ce50685f14940bac57976f
anbehindY <yanminthwin@gmail.com>
2023-05-23 22:20:00 +0630

    Add styling to details component

--- src/components/details.js
+++ src/components/details.js
@@ -1,9 +1,23 @@
+import { FaSearch } from 'react-icons/fa';
+import style from '../styles/details.module.css';
+import styles from '../styles/home.module.css';
+
 const Details = () => (
-  <div className="detailsContainer">
-    <div className="charactersContainer">
-      <div className="characters">
+  <div className={style.detailsContainer}>
+    <div className={styles.banner} />
+    <div className={styles.searchBar}>
+      <FaSearch color="#fff" />
+      <input type="text" placeholder="Search character" className={styles.searchInput} />
+    </div>
+    <div className={style.charactersContainer}>
+      <div className={style.characters}>
         <img src="" alt="character" />
-        <p>Jon Snow</p>
+        <p className={style.name}>Jon Snow</p>
+        <p>King of the north</p>
+      </div>
+      <div className={style.characters}>
+        <img src="" alt="character" />
+        <p className={style.name}>Jon Snow</p>
         <p>King of the north</p>
       </div>
     </div>
