aa28d320356274ea0fb1529a93ce2f762af32e53
anbehindY <yanminthwin@gmail.com>
2023-05-25 01:00:10 +0630

    Modify footer links

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,12 +5,12 @@ const Footer = () => (
     <p className={style.author}>
       ©️Alfie Yang
       {' '}
-      <a href="https://github.com/anbehindY" className={style.link}>@anbehindY</a>
+      <a href="https://github.com/anbehindY" target="_blank" className={style.link} rel="noreferrer">@anbehindY</a>
     </p>
     <p className={style.designCredit}>
       Creative Commons design by
       {' '}
-      <a href="https://www.behance.net/sakwadesignstudio" className={style.link}>Nelson Sakwa</a>
+      <a href="https://www.behance.net/sakwadesignstudio" target="_blank" className={style.link} rel="noreferrer">Nelson Sakwa</a>
     </p>
   </footer>
 );
